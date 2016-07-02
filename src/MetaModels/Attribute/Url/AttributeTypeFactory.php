adf9076ea006cd1458a0c4c70eae036b83307e65
cboelter <christopher@boelter.eu>
2016-07-02 22:53:32 +0200

    added missing author information

--- src/MetaModels/Attribute/Url/AttributeTypeFactory.php
+++ src/MetaModels/Attribute/Url/AttributeTypeFactory.php
@@ -10,6 +10,7 @@
  * @package    MetaModels
  * @subpackage AttributeUrl
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Christopher Boelter <christopher@boelter.eu>
  * @copyright  2012-2016 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0
  * @filesource
