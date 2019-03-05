624d981fbfd6acaa662eb3793b7e9de51562aa71
Mini Model <minimodel@metamodel.me>
2019-03-05 19:48:07 +0100

    Update PHPDoc file headers

--- tests/Test/DependencyInjection/MetaModelsAttributeUrlExtensionTest.php
+++ tests/Test/DependencyInjection/MetaModelsAttributeUrlExtensionTest.php
@@ -1,21 +1,20 @@
 <?php
 
 /**
- * * This file is part of MetaModels/attribute_url.
+ * This file is part of MetaModels/attribute_url.
  *
- * (c) 2012-2017 The MetaModels team.
+ * (c) 2012-2019 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage AttributeUrl
+ * @package    MetaModels/attribute_url
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  2012-2017 The MetaModels team.
- * @license    https://github.com/MetaModels/attribute_text/blob/master/LICENSE LGPL-3.0
+ * @copyright  2012-2019 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_url/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
