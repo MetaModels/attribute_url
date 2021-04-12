06fd0552dba1665770a79d3986b2ac0a9eaec960
Sven Baumann <baumann.sv@gmail.com>
2021-04-12 17:37:01 +0200

    Update phpunit tests

--- tests/Test/DeprecatedAutoloaderTest.php
+++ tests/Test/DeprecatedAutoloaderTest.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_url.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2021 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,7 +13,8 @@
  * @package    MetaModels/attribute_url
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  2012-2019 The MetaModels team.
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2012-2021 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_url/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -26,6 +27,9 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * This class tests if the deprecated autoloader works.
+ *
+ * @covers \MetaModels\AttributeUrlBundle\Attribute\Url
+ * @covers \MetaModels\AttributeUrlBundle\Attribute\AttributeTypeFactory
  */
 class DeprecatedAutoloaderTest extends TestCase
 {
@@ -65,11 +69,11 @@ class DeprecatedAutoloaderTest extends TestCase
      */
     public function testDeprecatedClassesAreAliased($oldClass, $newClass)
     {
-        $this->assertTrue(\class_exists($oldClass), \sprintf('Class url "%s" is not found.', $oldClass));
+        self::assertTrue(\class_exists($oldClass), \sprintf('Class url "%s" is not found.', $oldClass));
 
         $oldClassReflection = new \ReflectionClass($oldClass);
         $newClassReflection = new \ReflectionClass($newClass);
 
-        $this->assertSame($newClassReflection->getFileName(), $oldClassReflection->getFileName());
+        self::assertSame($newClassReflection->getFileName(), $oldClassReflection->getFileName());
     }
 }
